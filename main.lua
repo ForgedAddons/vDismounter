c2d1d9a29c9067b65b57aa4b1e6c06f278bf253e
V. <bartosz.bentkowski@gmail.com>
2014-01-04 13:01:49 +0100

    Use externals

--- main.lua
+++ main.lua
@@ -1,15 +1,13 @@
-local event = CreateFrame("Frame")
-event:SetScript("OnEvent", function(self, event, ...) self[event](self, event, ...) end)
-event:RegisterEvent"UI_ERROR_MESSAGE"
+local addon, ns = ...
 
-event.UI_ERROR_MESSAGE = function(self, event, ...)
+ns.RegisterEvent("UI_ERROR_MESSAGE", function(self, event, ...)
 	local err = ...;
 	local adf = GetCVarBool("autoDismountFlying");
-	if(err == SPELL_FAILED_NOT_MOUNTED or err == ERR_TAXIPLAYERALREADYMOUNTED) then
+	if (err == SPELL_FAILED_NOT_MOUNTED or err == ERR_TAXIPLAYERALREADYMOUNTED) then
 		if adf and IsFlying() then
 			return;
 		else
 			Dismount();
 		end
 	end
-end
+end)
